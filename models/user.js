14b2199c6d232d7ef8c6bd035b2c80e08d82fe03
Ashutosh Deshmukh <acdeshmukhvip@gmail.com>
2022-07-27 22:50:02 +0530

    Auth Code Connected to Ui

--- models/user.js
+++ models/user.js
@@ -1,11 +1,13 @@
-const mongoose = require("mongoose");
-var passwordLocalMongoose = require("passport-local-mongoose");
+const mongoose = require('mongoose');
+var passwordLocalMongoose = require('passport-local-mongoose');
 
 var UserSchema = new mongoose.Schema({
   username: String,
+  email: String,
+  role: String,
   password: String,
 });
 
 UserSchema.plugin(passwordLocalMongoose);
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const router = express.Router();
 const bp = require('body-parser');
 const qr = require('qrcode');
+const isEmailValid = require('email-validator');
+const passwordValidator = require('password-validator');
 
 const passport = require('passport');
 const User = require('../models/user');
 
 // Show login form on homepage
 router.get('/', (req, res) => {
+  res.render('login');
+});
+
+router.get('/login', (req, res) => {
   res.render('login');
 });
 
@@ -29,7 +35,9 @@ router.get('/item-manufactdash', (req, res) => {
   res.render('item-manufact-dash');
 });
 
-//handle login logic
+router.get('login-failed', (req, res) => {
+  res.render('login-failed');
+});
 // ----------> router.post('/login', middleware, callback)
 // router.post('/login', (req, res) => {
 //   const email = req.body.email;
@@ -41,26 +49,54 @@ router.get('/item-manufactdash', (req, res) => {
 //   }
 // });
 
+// handle login logic
 router.post(
   '/login',
   passport.authenticate('local', {
     successRedirect: '/dashboard',
-    failureRedirect: '/login',
+    failureRedirect: '/',
   }),
   (req, res) => {}
 );
 
 router.post('/register', (req, res) => {
-  var newUser = new User({ username: req.body.username });
-  User.register(newUser, req.body.password, (err, user) => {
-    if (err) {
-      console.log(err);
-      return res.render('register');
+  console.log(req.body);
+
+  var schema = new passwordValidator();
+  const password = req.body.password;
+  const confirmpassword = req.body.confirmpassword;
+  schema.is(password).min(8);
+  const email = req.body.email;
+
+  if (isEmailValid.validate(email)) {
+    if (schema.validate(password) && password == confirmpassword) {
+      var newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        role: req.body.role,
+      });
+
+      User.register(newUser, req.body.password, (err, user) => {
+        if (err) {
+          console.log(err);
+          return res.render('register', {
+            errormsg: 'Email Already Registered',
+          });
+        }
+        passport.authenticate('local')(req, res, () => {
+          res.redirect('/');
+        });
+      });
+    } else {
+      return res.render('register', {
+        errormsg: 'Password is Too Short / Passwords Do Not Match',
+      });
     }
-    passport.authenticate('local')(req, res, () => {
-      res.redirect('/posts');
+  } else {
+    return res.render('register', {
+      errormsg: 'This Email is Invalid',
     });
-  });
+  }
 });
 
 function isLoggedIn(req, res, next) {
@@ -72,6 +108,7 @@ function isLoggedIn(req, res, next) {
 
 //logout Logic
 router.get('/logout', (req, res) => {
+  req.logout();
   res.redirect('/');
 });
 
